a7926f251eb3e0d4c08dc09f03902254aa81802b
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-01-16 19:44:52 +0100

    Finished cat_kata_part1.java

--- kyu6/cat_kata_part1/cat_kata_part1.java
+++ kyu6/cat_kata_part1/cat_kata_part1.java
@@ -1,15 +1,23 @@
 public class PeacefulYard {
     public static boolean peacefulYard(String[] yard, int minDistance) 
     {
+      System.out.println(minDistance);
       int row_quantity = yard.length, col_quantity = yard[0].length(), cats = 0, row1 = 0, row2 = 0, col1 = 0, col2 = 0;
-      double distance = 10, new_distance;
+      double distance = 100, new_distance;
       boolean rowcol1 = true;
       String word;
-      for (int i = row_quantity - 1; i > 0; i--)
+      for (int i = row_quantity - 1; i >= 0; i--)
       {
         word = yard[i];
-        for (int j = col_quantity - 1; j > 0; j--)
+        System.out.println(word + i);
+        for (int j = 0; j < col_quantity; j++)
         {
+          if (row2 != 0 || col2 != 0)
+          {
+            new_distance = Math.sqrt((Math.pow((row2 - row1), 2) + Math.pow((col2 - col1), 2)));
+            distance = new_distance < distance ? new_distance : distance;
+            System.out.println("New_distance: " + new_distance + "..Distance: " + distance);
+          }
           if (word.charAt(j) != '-' && rowcol1)
           {
             row1 = i;
@@ -24,12 +32,6 @@ public class PeacefulYard {
             rowcol1 = true;
             System.out.println("2: " + word.charAt(j));
           }
-        }
-        if (row2 != 0 || col2 != 0)
-        {
-          new_distance = Math.sqrt((Math.pow((row2 - row1), 2) + Math.pow((col2 - col1), 2)));
-          distance = new_distance < distance ? new_distance : distance;
-          System.out.println("New_distance: " + new_distance + "..Distance: " + distance);
         }
       }
       if (row2 == 0 && col2 == 0)
