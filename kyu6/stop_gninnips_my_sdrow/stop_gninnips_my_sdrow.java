7c7e388a47926be0cc1fa7a4e1d216a76afbbd31
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-01-17 13:10:26 +0100

    Almost finished stop_gninnips_my_sdrow.java

--- kyu6/stop_gninnips_my_sdrow/stop_gninnips_my_sdrow.java
+++ kyu6/stop_gninnips_my_sdrow/stop_gninnips_my_sdrow.java
@@ -6,40 +6,58 @@ public class SpinWords {
     char letter;
     int letter_quantity = 0, last_word = 0;
     int length = sentence.length();
-    boolean end = false;
+    boolean end = false, spinned = false;
     for (int i = 0; i < length; i ++)
     {
       if (i == length - 1)
       {
         end = true;
+        System.out.println("End: " + end);
       }
       letter = sentence.charAt(i);
       if (letter != ' ' && !end)
       {
         letter_quantity++;
+        System.out.println("Letter quantity+: " + letter_quantity);
       }
       else
       {
         if (letter_quantity > 4)
         { 
+          if (spinned)
+          {
+            letter_quantity++;
+            System.out.println("Spinned: " + spinned);
+          }
           for (int k = letter_quantity; k >= 0; k--)
           {
             copy += sentence.charAt(k + last_word);
+            System.out.println(">4." + copy + ".");
           }
+          System.out.println("i: " + i);
+          spinned =true;
         }
         else
         {
           for (int j = last_word; j < i; j++)
           {
             copy += sentence.charAt(j);
+            System.out.println("." + copy +".");
           }
         }
-        if (!end && letter_quantity != 0)
+        if (!end && letter_quantity != 0 && !spinned)
         {
           copy += " ";
+          System.out.println(".." + copy + "..");
         }
         letter_quantity = 0;
+        if (spinned)
+        {
+          i++;
+        }
         last_word = i;
+        spinned = false;
+        System.out.println("Letere + last word: " + letter_quantity + " " + last_word);
       }
     }
     return copy;
