66328d48f0860b5c24e7052afe705f53c5f8f27d
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-01-22 12:18:25 +0100

    Updating merging_sorted_integer_arrays.java

--- kyu8/merging_sorted_integer_arrays/merging_sorted_integer_arrays.java
+++ kyu8/merging_sorted_integer_arrays/merging_sorted_integer_arrays.java
@@ -3,19 +3,31 @@ import java.util.Arrays;
 public class Kata {
 	public static int[] mergeArrays(int[] first, int[] second) 
   {
-    int i = 0, new_length = first.length + second.length;    
-		int [] result = new int[new_length];
+    int i = 0, total_length = first.length + second.length, result_length = 0;   
+		int[] result_unsorted = new int[total_length];
     for (i = 0; i < first.length; i++)
     {
-      result[i] = first[i];  
+      result_unsorted[i] = first[i];  
     }
     for (int j = 0; i < second.length; j++)
     {
-      if(check_duplicate(result, second[j]))
+      if(check_duplicate(result_unsorted, second[j]))
       {
         continue;
       }
-      result[i + j] = second[j];
+      result_unsorted[i + j] = second[j];
+    }
+    for (int l = 0; l < total_length; l++)
+    {
+      if (result_unsorted[l] == 0)
+      {
+        result_length = l;
+      }
+    }
+    int[] result = new int[result_length];
+    for (int m = 0; m < result_length; m++)
+    {
+      result[m] = result_unsorted[m];
     }
     Arrays.sort(result);
     return result;
