e9d0c5a7f0743a504e392933cf2465aafee89bd8
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-01-23 18:01:00 +0100

    Completed determine_if_the_poker_hand_is_flush.java

--- kyu7/determine_if_the_poker_hand_is_flush/determine_if_the_poker_hand_is_flush.java
+++ kyu7/determine_if_the_poker_hand_is_flush/determine_if_the_poker_hand_is_flush.java
@@ -1,1 +1,22 @@
-//Code here
+public class Kata
+{
+  public static boolean CheckIfFlush(String[] cards)
+  {
+    int hand_length = cards.length, card_length;
+    boolean flush = true;
+    char suit = '\n';
+    for (int i = 0; i < hand_length; i++)
+    {
+      card_length = cards[i].length();
+      if (i == 0)
+      {
+        suit = cards[i].charAt(card_length - 1);
+      }
+      else if (cards[i].charAt(card_length - 1) != suit)
+      {
+        flush = false;
+      }
+    }
+    return flush; 
+  }
+}
