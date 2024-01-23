f4cb278f064395942b083fda4514c7a0dce7b01b
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-01-23 20:26:25 +0100

    Completed simple_fun_2_circle_of_numbers.java

--- kyu7/simple_fun_2_circle_of_numbers/simple_fun_2_circle_of_numbers.java
+++ kyu7/simple_fun_2_circle_of_numbers/simple_fun_2_circle_of_numbers.java
@@ -1,1 +1,16 @@
-//Code here
+public class CircleOfNumbers 
+{
+    public static int circleOfNumbers(int n, int firstNumber) 
+    {
+      int middle = n / 2;
+      int move = firstNumber;
+      int result = middle + move;
+      if (result < n)
+      {
+        return result;
+      }     
+      int laps = (result / n);
+      int position = result - (laps * n);
+      return position;
+    }
+}
