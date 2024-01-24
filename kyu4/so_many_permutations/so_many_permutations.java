83817cb4b7ec26f25e2d87acb6dcd92f5d1090be
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-01-24 13:51:52 +0100

    Update so_many_permutations.java

--- kyu4/so_many_permutations/so_many_permutations.java
+++ kyu4/so_many_permutations/so_many_permutations.java
@@ -2,28 +2,40 @@ import java.util.List;
 
 class Permutations 
 {   
-    public static List<String> singlePermutations(String s) 
+  public static List<String> singlePermutations(String s) 
+  {
+    List<char> letters = new List<char>();
+    int length = s.length;
+    for (int i = 0; i < length; i++)
     {
-      if(n == 1) 
-      {
-        printArray(elements, delimiter);
-      }
-      else 
-      {
-         for(int i = 0; i < n-1; i++) 
-         {
-           printAllRecursive(n - 1, elements, delimiter);
-           if(n % 2 == 0) 
-           {
-             swap(elements, i, n-1);
-           }
-           else
-           {
-             swap(elements, 0, n-1);
-           }
-         }
-        printAllRecursive(n - 1, elements, delimiter);
-      }
-    return null;
+      letter.add(i, s.charAt(i));
     }
+    permutations(length);
+    return null;
+  }
+
+  public static List<String> permutations(int length, List<char> characters)
+  {
+    if(length == 1) 
+    {
+      list_add(elements);
+    }
+    else 
+    {
+       for(int i = 0; i < length - 1; i++) 
+       {
+         permutations(length - 1, character);
+         if(n % 2 == 0) 
+         {
+           swap(elements, i, length - 1);
+         }
+         else
+         {
+           swap(elements, 0, length -  1);
+         }
+       }
+      permutations(length - 1, character);
+    }
+    return null;
+  }
 }
