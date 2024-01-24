3e4616544005ae272d9ede58a677d78c01ee8dbb
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-01-24 13:01:20 +0100

    Completed count_the_smiley_faces.java

--- kyu6/count_the_smiley_faces/count_the_smiley_faces.java
+++ kyu6/count_the_smiley_faces/count_the_smiley_faces.java
@@ -1,1 +1,43 @@
-//Code here
+import java.util.*;
+
+public class SmileFaces 
+{  
+  public static int countSmileys(List<String> arr) 
+  {
+    int size = arr.size();
+    int length, smileys = 0;
+    String element;
+    char character;
+    boolean eyes = false, nose = false;
+    for (int i = 0; i < size; i++)
+    {
+      element = arr.get(i);
+      length = element.length();
+      for (int j = 0; j < length; j++)
+      {
+        character = element.charAt(j);
+        if (!eyes && (character == ':' || character == ';'))
+        {
+          eyes = true;
+        }
+        else if (eyes && (character == ')' || character == 'D') && (j == length - 1))
+        {
+          smileys++;
+          break;
+        }
+        else if (eyes && (character == '-' || character == '~') && !nose)
+        {
+          nose = true;
+          continue;
+        }
+        else
+        {
+          break;
+        }
+      }
+      eyes = false;
+      nose = false;
+    }
+    return smileys;
+  }
+}
