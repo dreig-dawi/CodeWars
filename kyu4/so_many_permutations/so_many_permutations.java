1377423fdd9f481715f56dc7257bc54e5065dff5
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-01-25 16:13:18 +0100

    Completed so_many_permutations.java

--- kyu4/so_many_permutations/so_many_permutations.java
+++ kyu4/so_many_permutations/so_many_permutations.java
@@ -9,8 +9,12 @@ class Permutations
   
   public static List<String> singlePermutations(String s) 
   {
-    permutation("", s);
-    System.out.println("REsult: " + result);
+    //To clear last results
+    result.clear();
+    
+    permutation(s, "");
+    
+    //Removing strings with a minor length than 's'
     result.removeIf(n -> (n.length() != s.length()));
     
     //Removing duplicates
@@ -20,20 +24,27 @@ class Permutations
     return result;
   }
   
-  public static void permutation(String prefix, String str) 
-  {
-    int n = str.length();
-    if (n == 0) 
-    {      
-      result.add(prefix);
-      System.out.println(prefix);
+  public static void permutation(String str, String ans) 
+   {
+ 
+        // If string is empty
+        if (str.length() == 0) {
+            result.add(ans);
+            return;
+        }
+ 
+        for (int i = 0; i < str.length(); i++) {
+ 
+            // ith character of str
+            char ch = str.charAt(i);
+ 
+            // Rest of the string after excluding
+            // the ith character
+            String ros = str.substring(0, i) +
+                        str.substring(i + 1);
+ 
+            // Recursive call
+            permutation(ros, ans + ch);
+        }
     }
-    else 
-    {
-      for (int i = 0; i < n; i++)
-      {            
-        permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
-      }
-    }
-  }
 }
