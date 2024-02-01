bc7953004ba01f9f6e9637f5e2ddc4e03e5880ec
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-02-01 13:32:53 +0100

    Completed simple_fun_319_number_and_ip_address.java

--- kyu6/simple_fun_319_number_and_ip_address/simple_fun_319_number_and_ip_address.java
+++ kyu6/simple_fun_319_number_and_ip_address/simple_fun_319_number_and_ip_address.java
@@ -5,12 +5,12 @@ public class NumberAndIPAddress
     String number = "", result;
     char letter;
     int length = s.length();
-    long numeric_result;
+    System.out.println(s);
     
     //Checking if it is an IP
-		if (s.matches("^(.*?([.])*)$"))
+		if (s.contains("."))
     {
-      int binary1 = 0, binary2 = 0, binary3 = 0;
+      String binary1 = "", binary2 = "", binary3 = "";
       int dots = 0;
       for (int i = 0; i < length; i++)
       {
@@ -21,13 +21,13 @@ public class NumberAndIPAddress
           switch (dots)
           {
               case 1:
-                binary1 = Integer.valueOf(number);
+                binary1 = Integer.toBinaryString(Integer.valueOf(number));
                 break;
               case 2:
-                binary2 = Integer.valueOf(number);
+                binary2 = Integer.toBinaryString(Integer.valueOf(number));
                 break;
               case 3:
-                binary3 = Integer.valueOf(number);
+                binary3 = Integer.toBinaryString(Integer.valueOf(number));
           }
           number = "";
         }
@@ -36,13 +36,89 @@ public class NumberAndIPAddress
           number += letter;
         }
       }
-      result = binary1 + binary2 + binary3 + number;
-      numeric_result = Long.parseLong(result, 2);
-      result = "" + numeric_result;
-      return result;      
+      number = Integer.toBinaryString(Integer.valueOf(number));
+      
+      //Making sure binary has 8 digits
+      binary1 = digits_8(binary1);
+      binary2 = digits_8(binary2);
+      binary3 = digits_8(binary3);
+      number = digits_8(number);
+      
+      result ="" + binary1 + binary2 + binary3 + number;
+      
+      //Parse long to String
+      return "" + to_decimal(result);
     }
     
-    System.out.println("False");
-    return s;
+    //Number introduced
+    else
+    {
+      String decimal1 = "", decimal2 = "", decimal3 = "", decimal4 = "";
+      char digit;
+      
+      //Making sure result has 32 digits
+      result = digits_32(Long.toBinaryString(Long.valueOf(s)));
+      System.out.println(result);
+      
+      //Loop through s, stoping every 8 digits
+      for (int i = 0; i < 4; i++)
+      {
+        for (int j = i * 8; j < 8 + (i * 8); j++)
+        {
+          digit = result.charAt(j);
+          switch (i)
+          {
+              case 0:
+                decimal1 += digit;
+                break;
+              case 1:
+                decimal2 += digit;
+                break;
+              case 2:
+                decimal3 += digit;
+                break;
+              case 3:
+              decimal4 += digit;
+          }
+        }
+      }
+      result = to_decimal(decimal1) + "." + to_decimal(decimal2) + "." + to_decimal(decimal3) + "." +to_decimal(decimal4);
+      return result;
+    }
+    
   }
+  
+  public static String digits_8(String binary)
+  {
+    int length = binary.length();
+    if (length < 8)
+    {
+      int add = 8 - length;
+      for (int i = 0; i < add; i++)
+      {
+        binary = "0" + binary;
+      }
+    }
+    return binary;
+  }
+  
+  public static String digits_32(String binary)
+  {
+    int length = binary.length();
+    if (length < 32)
+    {
+      int add = 32 - length;
+      for (int i = 0; i < add; i++)
+      {
+        binary = "0" + binary;
+      }
+    }
+    return binary;
+  }
+  
+  public static long to_decimal(String binary)
+  {
+    return Long.parseLong(binary, 2);
+  }
+
 }
