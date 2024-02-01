0d8d06b51cb9f66a4fe97f06aa5f3b2674d33277
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-02-01 15:57:06 +0000

    Creating encrypt_this Kata

---
+++ kyu6/encrypt_this/encrypt_this.java
@@ -0,0 +1,38 @@
+public class Kata 
+{
+    public static String encryptThis(String text) 
+    {
+      String result = "";
+      int length = text.length();
+      int word = 0;
+      char character;
+      boolean word_check = false;
+      if (length != 0)
+      {
+        for (int i = 0; i < length; i++)
+        {
+          if (!word_check)
+          {
+            word = i;
+            word_check = true;
+          }
+          else if (word_check && (text.charAt(i) == ' ' || i == length - 1))
+          {
+            word_check = false;
+            result += (int)text.charAt(word);
+            if (i - word > 1)
+            {              
+              result += text.charAt(i - 1);
+              for (int j = word + 2; j < i - 1; j++)
+              {
+                result += text.charAt(j);
+              }
+              result += text.charAt(word + 1);
+            }
+            result += " ";
+          }
+        }
+      }
+      return result;
+    }
+}
