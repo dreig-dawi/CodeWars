431b89f2c1dfce1c6a9ac877440c4099679baf94
dreig-dawi <146749815+dreig-dawi@users.noreply.github.com>
2024-02-07 16:26:35 +0100

    Only missing types pascals_trianlge.java

--- kyu6/pascals_triangle/pascals_trianlge.java
+++ kyu6/pascals_triangle/pascals_trianlge.java
@@ -1,40 +1,62 @@
-import java.util.ArrayList
+import java.util.ArrayList;
 
 public class PascalsTriangle 
 {
     public static long[] generate(int level) 
     {
-      ArrayList<Long> result_list = new ArrayList<Long>;
+      
+      //Introduce result into results_list
+      ArrayList<Double> result_list = new ArrayList<Double>();
       for (int i = 0; i < level; i++)
       {
         if (i == 0)
         {
-          result_list.add(1);
+          result_list.add((double)1);
         }
         else if (i == 1)
         {
-          result_list.add(1);
-          result_list.add(1);
+          result_list.add((double)1);
+          result_list.add((double)1);
         }
         else
         {
-          for (int k = 0; k < i + 1; i++)
+          for (long k = 0; k < i + 1; k++)
           {
-            result_list.add(factorial(level))
+            result_list.add(binomial_coefficient(i, k));
+            System.out.println(binomial_coefficient(i, k) + "..Destrozo");
           }
         }
       }
       
-      return new long[0];
+      //Turning result_list(ArrayList<Long>) into result(long[])
+      long[] result = new long[result_list.size()];
+      for (int i = 0; i < result.length; i++)
+      {
+        result[i] = result_list.get(i);
+        System.out.println("result(long[]): "+result[i]);
+      }
+      
+      System.out.println(result);
+      return result;
     }
   
-    public static int factorial(int number)
+    public static double factorial(long number)
     {
-      int factorial = 1;
-      for (int i = number; i > 0; i--)
+      long factorial = 1;
+      for (long i = number; i > 0; i--)
       {
         factorial *= i;
       }
-      return factorial
+      System.out.println("FACTORIAL: " + factorial);
+      return factorial;
+    }
+  
+    public static double binomial_coefficient(double n, double k)
+    {
+      System.out.println("N: "+n+"..K: "+k);
+      System.out.println("Nominator: " + factorial(n));
+      System.out.println("DENominator: " + (factorial(k) * factorial(n - k)));
+      System.out.println("DESTROZO RESULT: " + factorial(n) / (factorial(k) * factorial(n - k)));
+      return factorial(n) / (factorial(k) * factorial(n - k));
     }
 }
